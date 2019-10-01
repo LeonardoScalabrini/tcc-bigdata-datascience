5987acf2a2e3e808330302c4bf346ab99cba6c09
Leonardo <leonardo_scalabrini@hotmail.com>
2019-09-30 22:39:52 -0300

    Implementado lógica para recuperar arquivo passado

--- src/test/java/changedistiller/BananaTest.java
+++ src/test/java/changedistiller/BananaTest.java
@@ -38,22 +38,59 @@ public class BananaTest {
     public void testOld(){
         String diff = FileUtil.loadJava("diff_map.txt");
 
-        String pattern = Pattern.quote("\\" + "n");
+        String pattern = Pattern.quote("\r\n");
         List<String> list = Arrays.asList(diff.split(pattern));
         System.out.println(list);
 
         String newJava = FileUtil.loadJava("MapMaker_new.java");
 
         List<String> news = new ArrayList<>(Arrays.asList(newJava.split("\r\n")));
+
+        Integer start = 0;
+        Integer removed = 0;
         for (String s : list) {
-            if (s.startsWith("+")) {
-                String subString = s.substring(1, s.length());
-                news.remove(subString);
+
+            if (s.startsWith("-"))
+                continue;
+
+            if (s.startsWith("@@")){
+                start = Integer.valueOf(s.substring(s.indexOf("+"), s.lastIndexOf(",")));
+                start -= removed;
+                start--;
             }
+
+            if (s.startsWith("+")) {
+                news.remove(start - 1);
+                removed++;
+                continue;
+            }
+
+            start++;
+        }
+
+        start = 0;
+        for (String s : list) {
+
+            if (s.startsWith("+"))
+                continue;
+
+            if (s.startsWith("@@")){
+                start = Integer.valueOf(s.substring(s.indexOf("-") + 1, s.indexOf(",")));
+                start--;
+            }
+
+            if (s.startsWith("-"))
+                news.add(start - 1, s.substring(1, s.length()));
+
+            start++;
         }
 
         String old = FileUtil.loadJava("MapMaker_old.java");
 
-        Assert.assertEquals(Arrays.asList(old.split("\r\n")), news);
+        String generate = "";
+        for (String n : news) {
+            generate += n + "\r\n";
+        }
+        Assert.assertEquals(Arrays.asList(old.split("\r\n")).toString().replace("  ", ""), news.toString().replace("  ", ""));
     }
 }
